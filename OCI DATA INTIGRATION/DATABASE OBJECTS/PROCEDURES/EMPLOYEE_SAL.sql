43afaea3d4670c5ae38047890afd729d3a53ea01
tyagiocp <tyagiocp@gmail.com>
2026-05-17 20:53:35 +0530

    PROECDURE2

---
+++ OCI DATA INTIGRATION/DATABASE OBJECTS/PROCEDURES/EMPLOYEE_SAL.sql
@@ -0,0 +1,54 @@
+--------------------------------------------------------
+--  DDL for Procedure EMPLOYEE_SAL
+--------------------------------------------------------
+set define off;
+
+  CREATE OR REPLACE EDITIONABLE PROCEDURE "ODI_WS"."EMPLOYEE_SAL" (p_uniquebatchid IN VARCHAR2) AS
+CURSOR cur_rev IS
+SELECT
+    uniqebatchid,
+    salarydate,
+    company,
+    empid,
+    basic,
+    hra,
+    medical,
+    specialallow,
+    otherallow,
+    telephonerem,
+    conveyance
+FROM
+    emp_stg_salary where uniqebatchid=p_uniquebatchid;
+	BEGIN
+	    FOR i IN cur_rev LOOP
+		INSERT INTO emp_nom_salary( uniqebatchid,
+    salarydate,
+    company,
+    empid,
+    basic,
+    hra,
+    medical,
+    specialallow,
+    otherallow,
+    telephonerem,
+    conveyance)
+	values(
+	i.uniqebatchid,
+    i.salarydate,
+    i.company,
+    i.empid,
+    i.basic,
+    i.hra,
+    i.medical,
+    i.specialallow,
+    i.otherallow,
+    i.telephonerem,
+    i.conveyance);
+        END LOOP;
+EXCEPTION
+WHEN no_data_found THEN
+ raise_application_error(-20102, 'no_data_found' || 'Error!');
+        RAISE;
+		end;
+
+/
