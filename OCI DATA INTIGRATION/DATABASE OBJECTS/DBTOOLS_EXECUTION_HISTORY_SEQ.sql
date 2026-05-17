bd72b3ba0fbb0d31b851cebc935345bfa84a7b49
tyagiocp <tyagiocp@gmail.com>
2026-05-17 20:55:46 +0530

    SEQUENCE

---
+++ OCI DATA INTIGRATION/DATABASE OBJECTS/DBTOOLS_EXECUTION_HISTORY_SEQ.sql
@@ -0,0 +1,5 @@
+--------------------------------------------------------
+--  DDL for Sequence DBTOOLS$EXECUTION_HISTORY_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ODI_WS"."DBTOOLS$EXECUTION_HISTORY_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 61 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
